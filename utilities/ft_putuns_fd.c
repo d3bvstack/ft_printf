87ebcadd74682a02660a287ef107bbccae405df2
d3bvstack <180553755+d3bvstack@users.noreply.github.com>
2025-03-13 11:29:34 +0100

    feat: Improve ft_putuns_fd function to correctly count the number of digits

--- utilities/ft_putuns_fd.c
+++ utilities/ft_putuns_fd.c
@@ -6,7 +6,7 @@
 /*   By: dbarba-v <dbarba-v@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 08:30:01 by codespace         #+#    #+#             */
-/*   Updated: 2025/02/13 09:34:19 by dbarba-v         ###   ########.fr       */
+/*   Updated: 2025/02/13 11:09:50 by dbarba-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@ int	ft_putuns_fd(unsigned int n, int fd)
 {
 	int	i;
 
-	i = ft_count(n);
+	i = 0;
 	if (n > 9)
 	{
 		ft_putnbr_fd(n / 10, fd);
 	}
-	ft_putchar_fd(n % 10 + '0', fd);
+	i += ft_putchar_fd(n % 10 + '0', fd);
 	return (i);
 }
